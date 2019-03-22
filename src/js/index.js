29537c739d663424f5fa063063386c0fa65693eb
Ihor <maryniuk94@ukr.net>
2019-03-22 17:57:00 +0200

    switch animated burger, scroll-smooth for section, animate lib aos

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,18 @@
 import $ from 'jquery';
 import '../../node_modules/jquery/dist/jquery.slim.min';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+/*ANIMATE*/
+import '../../node_modules/aos/dist/aos.css';
+import '../../node_modules//aos/dist/aos.js';
+import './animate.js';
+/* END ANIMATE */
 import '../../node_modules/swiper/dist/css/swiper.min.css';
 import '../../node_modules/swiper/dist/js/swiper.min';
 import '../../node_modules/@fortawesome/fontawesome-free/js/all.min';
 import '../css/main.scss';
 import './scroll-nav';
 import './swiper-partners';
-import './test'; /*always needs slash before name*/
+
 
 
 
